0f390a44dd8cd5ee0457ec1ab44aca1a6f143104
raja1068 <masoomindia7@gmail.com>
2026-05-05 01:24:50 +0530

    Create interfaces.ts

---
+++ ludo-master/src/state/interfaces.ts
@@ -0,0 +1,34 @@
+import { ForkEffect } from 'redux-saga/effects';
+
+import { IState as IDiceState } from 'containers/Dice/state/interfaces';
+import { IState as ILudoState } from 'containers/Ludo/state/interfaces';
+import { IState as IContextMenuState } from 'services/contextMenu/interfaces';
+
+export enum BaseColors {
+  RED = 'RED',
+  GREEN = 'GREEN',
+  BLUE = 'BLUE',
+  YELLOW = 'YELLOW',
+}
+
+export enum WalkwayPosition {
+  NORTH = 'NORTH',
+  EAST = 'EAST',
+  WEST = 'WEST',
+  SOUTH = 'SOUTH',
+}
+
+export interface IApplicationState {
+  ludo: ILudoState;
+  contextMenu: IContextMenuState;
+  dice: IDiceState;
+}
+
+export interface IReduxAction<T = any, D = any> {
+  type: T;
+  data?: D;
+}
+
+export interface ISaga {
+  (): IterableIterator<ForkEffect>;
+}
