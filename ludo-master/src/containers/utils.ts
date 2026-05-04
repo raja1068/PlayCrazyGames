2d6d6191ece06daf806b559561b3351a87fdf872
raja1068 <masoomindia7@gmail.com>
2026-05-05 01:12:26 +0530

    Create utils.ts

---
+++ ludo-master/src/containers/utils.ts
@@ -0,0 +1,15 @@
+import { BaseColors } from 'state/interfaces';
+
+import { BASE_COLORS, CELL_DIMENSION } from 'globalConstants';
+
+export const getStyleObject = (
+  cellCountLengthwise: number,
+  cellCountWidthwise: number,
+  baseColor?: BaseColors,
+): React.CSSProperties => ({
+  backgroundColor: baseColor && getBaseHexColor(baseColor),
+  height: `${cellCountWidthwise * CELL_DIMENSION}px`,
+  width: `${cellCountLengthwise * CELL_DIMENSION}px`,
+});
+
+export const getBaseHexColor = (color: BaseColors) => BASE_COLORS[color];
