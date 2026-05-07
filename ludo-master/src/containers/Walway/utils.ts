36d30ac6de8679ee644404bfe8095366a0c49435
raja1068 <masoomindia7@gmail.com>
2026-05-08 01:46:48 +0530

    Delete ludo-master directory

--- ludo-master/src/containers/Walway/utils.ts
+++
@@ -1,3 +0,0 @@
-import { WalkwayPosition } from 'state/interfaces';
-
-export const generateCellID = (walkwayPosition: WalkwayPosition, row: number, column: number) => `${walkwayPosition}_${row}_${column}`;
